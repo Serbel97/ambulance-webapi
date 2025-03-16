f1b4e612f0ecc3ae12bb6df633c67bc3fca02645
Erik Belak <erik.belak@gmail.com>
2025-03-16 20:12:41 +0100

    Add mongodb compose file

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,13 +9,37 @@ PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 export AMBULANCE_API_ENVIRONMENT="Development"
 export AMBULANCE_API_PORT="8080"
 
+export AMBULANCE_API_MONGODB_USERNAME="root"
+export AMBULANCE_API_MONGODB_PASSWORD="neUhaDnes"
+
+# Define a helper function for docker compose using our compose file
+mongo() {
+    docker compose --file "${PROJECT_ROOT}/deployments/docker-compose/compose.yaml" "$@"
+}
+
 # Execute the corresponding command
 case "$command" in
   start)
+    # "Try" block:
+    # Bring up MongoDB containers in detached mode.
+    mongo up --detach
+
+    # Set a trap that will run when the script exits (normal or error),
+    # simulating a "finally" block to ensure MongoDB is shut down.
+    trap 'mongo down' EXIT
+
+    # Run the API service.
     go run "${PROJECT_ROOT}/cmd/ambulance-api-service"
+    # When this script exits (even due to an error), the trap will execute
+    # and shut down the MongoDB containers.
     ;;
   openapi)
     docker run --rm -ti -v "${PROJECT_ROOT}":/local openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
+    ;;
+  mongo)
+    # Shift removes the "mongo" command so the rest of the arguments go to our helper.
+    shift
+    mongo "$@"
     ;;
   *)
     echo "Unknown command: $command" >&2
